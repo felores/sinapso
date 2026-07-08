12855ecbf83939d1776de873cb5c700388461a24
Felo Restrepo <44730261+felores@users.noreply.github.com>
2026-07-08 07:11:56 -0500

    lowercase url in english placeholder too

--- web/src/i18n.ts
+++ web/src/i18n.ts
@@ -130,7 +130,7 @@ const EN: Dict = {
   "search.ph.none": "Search notes…",
   "search.ph.vault": "Search vault…",
   "search.ph.web": "Web research…",
-  "search.ph.ingest": "Ingest file/URL…",
+  "search.ph.ingest": "Ingest file/url…",
   "mode.vault.name": "Vault",
   "mode.web.name": "Web",
   "mode.ingest.name": "Ingest",
